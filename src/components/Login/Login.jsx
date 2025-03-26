bac86a96d74764f24766f1571cc0510efc788800
jnakht <jisan.cse.cu@gmail.com>
2025-03-26 15:14:58 +0600

    login and register component done without backend

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,39 @@
 
 const Login = () => {
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const userEmail = e.target.email.value;
+        const userPassword = e.target.password.value;
+        console.log(userEmail, userPassword);
+    }
     return (
-        <div>
-            
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col ">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold mb-4">Login now!</h1>
+
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <form onSubmit={handleLogin}>
+                        <div className="card-body">
+                            <fieldset className="fieldset">
+                                <label className="fieldset-label">Email</label>
+                                <input type="email" 
+                                name='email'
+                                className="input" placeholder="Email" 
+                                required/>
+                                <label className="fieldset-label">Password</label>
+                                <input type="password" 
+                                name='password'
+                                className="input" placeholder="Password" 
+                                required/>
+                                <div><a className="link link-hover">Forgot password?</a></div>
+                                <button className="btn btn-primary mt-4">Login</button>
+                            </fieldset>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
