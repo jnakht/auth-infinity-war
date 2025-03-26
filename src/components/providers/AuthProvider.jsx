22e451da97fcb89e134c987fa4b025d3f523572f
jnakht <jisan.cse.cu@gmail.com>
2025-03-26 16:58:48 +0600

    login with email and password

--- src/components/providers/AuthProvider.jsx
+++ src/components/providers/AuthProvider.jsx
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { createContext, useState } from "react";
 import auth from "../../firebase/firebase.config";
 
@@ -8,7 +8,10 @@ const AuthProvider = ({children}) => {
     const createUser = (email, password) => {
        return  createUserWithEmailAndPassword(auth, email, password);
     }
-    const authInfo = {user, createUser};
+    const logInUser = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+    const authInfo = {user, createUser, logInUser};
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
